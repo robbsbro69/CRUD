5c758f6af0b4bcef70e57cb24c119576ce532a18
robbsbro69 <robbsbro369@proton.me>
2025-07-23 23:26:08 +0545

    configuring models

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -9,7 +9,7 @@ const productSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
-    img : {
+    image : {
         type: String,
         required: true
     },
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,37 @@ import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './configs/db.js';
 import Product from './models/product.model.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
-const  app = express();
+const app = express();
+app.use(express.json());
 
-app.post('/products', async (req,res) => {
+app.get("/api/products", async(req,res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({
+            success: true, 
+            data: products
+        });
+    } catch (error) {
+        console.log("Error in fetching products", error.message);
+        res.status(500).json({
+            success: false, 
+            message: "Server error"
+        });
+    }
+})
+
+app.post('/api/products', async (req,res) => {
     const product = req.body; 
 
-    if(!product.name || product.price || product.image){
-        return res.status(400).json({success: false, message: "Please provide all details."});
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({
+            success: false, 
+            message: "Please provide all details."
+        });
     }
 
     const newProduct = new Product(product)
@@ -21,13 +42,54 @@ app.post('/products', async (req,res) => {
         res.status(201).json({success: true, data: newProduct});
     } catch (error) {
         console.error(`Error in Creating product ${error.message}`);
-        res.status(500).json({success: false, message: "Server error"});
+        res.status(500).json({
+            success: false, 
+            message: "Server error"
+        });
+    }
+});
+
+app.put("/api/products/:id", async(req,res) => {
+    const {id} = req.params;
+
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({
+            success: false, 
+            message:"Invalid product id"
+        });
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new: true});
+        res.status(200).json({success: true, data: updatedProduct});
+    } catch (error) {
+        res.status(500).json({success: false, message: "Sever error"});
+    }
+}) 
+
+app.delete('/api/products/:id', async(req,res) => {
+    const {id} = req.params;
+    
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({
+            success: true,
+            message: "Product delete"
+        });
+    } catch (error) {
+        console.log("Error while deleting product", error.message)
+        res.status(404).json({
+            success: false,
+            message: "Error deleting product"
+        });
     }
 });
 
 app.get('/', (req,res) => {
     res.send("Server is ready");
-})
+});
 
 app.listen(5000, () => {
     connectDB();
